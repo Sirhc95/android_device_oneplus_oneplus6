c044022deb994d9a6c656551b4fbaa07ea65efb8
Chris Crump <chris@cypheros.co>
2020-11-05 12:59:19 -0500

    oneplus6: lights: Move to .bp

--- lights/Android.mk
+++ lights/Android.mk
@@ -2,34 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_sdm845
-LOCAL_MODULE_TAGS  := optional
-
-LOCAL_VENDOR_OVERLAY_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service
-
-LOCAL_SRC_FILES := \
-    service.cpp \
-    Light.cpp
-
-LOCAL_REQUIRED_MODULES := \
-    android.hardware.light@2.0-service.oneplus_sdm845.rc
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware \
-    libhidlbase \
-    liblog \
-    android.hardware.light@2.0
-
-LOCAL_STATIC_LIBRARIES := \
-    libbase \
-    libutils
-
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
 LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_sdm845.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
