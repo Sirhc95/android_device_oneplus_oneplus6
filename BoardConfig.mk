e7269dbfdc9e57875deac29f5e676a09ebd9b458
Chris Crump <chriscrawford893@gmail.com>
2022-01-02 21:27:53 -0500

    oneplus6: Add a vendor framework compatibility matrix
    Added the required HALs that don't pass Vintf checks

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
