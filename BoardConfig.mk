9ee3e848fbfee9973d4a0db562223c3709ea5f34
Chris Crump <chriscrawford893@gmail.com>
2022-01-02 21:33:00 -0500

    oneplus6: Set selinux to Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
     androidboot.console=ttyMSM0 \
+    androidboot.selinux=permissive \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     msm_rtb.filter=0x237 \
     ehci-hcd.park=3 \
