bac6c1f8c49af40651ad7bba79fcc510d1fadc2d
Chris Crump <chriscrawford893@gmail.com>
2022-01-17 21:07:47 -0500

    oneplus6: Build vendor variant of NFC HAL
    CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.nfc@1.2-service": library "android.hardware.nfc@1.0.so" not found: needed by main executable

--- oneplus6.mk
+++ oneplus6.mk
@@ -333,6 +333,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0 \
     android.hardware.nfc@1.1 \
     android.hardware.nfc@1.2 \
+    android.hardware.nfc@1.2.vendor \
     android.hardware.secure_element@1.0 \
     android.hardware.secure_element@1.1 \
     android.hardware.secure_element@1.0.vendor \
